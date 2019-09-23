0ce82e11cf9cc1d2cee05ff6c9e54aa80319b1dd
Michael Curry <mdcurry1138@gmail.com>
2019-09-23 10:21:01 -0400

    Baseline Flask App

---
+++ App_TESS/__init__.py
@@ -0,0 +1,4 @@
+"""Entry point for TESS planet finding Flask App"""
+from .app import create_app
+
+APP = create_app()
---
+++ App_TESS/app.py
@@ -0,0 +1,22 @@
+"""Main Application and routing Logic for TESS Flask App"""
+from decouple import config
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+
+
+def create_app():
+    """create and config an instance of the Flask App"""
+    app = Flask(__name__)
+
+    # configure DB, will need to update this when changing DBs?
+    app.config['SQLALCHEMY_DATABASE_URI'] = config('DATABASE_URL')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['ENV'] = config('ENV')
+
+    # Create home route
+    @app.route('/')
+    def root():
+        return render_template('home.html', title = 'Home')
+
+    return app
+    
