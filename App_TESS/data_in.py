10fb333c6758830ec0b0cb70df5248044b36cfa6
Michael Curry <mdcurry1138@gmail.com>
2019-09-26 09:53:26 -0400

    I might have a nested repo here?

---
+++ App_TESS/data_in.py
@@ -0,0 +1,188 @@
+import pandas as pd
+from flask_sqlalchemy import SQLAlchemy
+from astroquery.mast import Catalogs, Observations
+from astropy.table import Table
+from tqdm import tqdm                       # Will need to remove this througout
+from .models import *
+
+
+# fetch TIC IDs from caltech
+def get_visual_data():
+    try:
+        # Getting labelled TESS Objects of Interest dataframe from Caltech:
+        toi = pd.read_csv('https://exofop.ipac.caltech.edu/tess/' + 
+                  'download_toi.php?sort=toi&output=csv')
+        # Isolating TIC IDs and TFOPWG Disposition values to use as target:
+        toi = toi[['TIC ID', 'TFOPWG Disposition']]
+        toi = toi.rename(columns={'TIC ID': 'TIC_ID'})
+
+        # Getting additional data on TESS Objects of Interest from STScI:
+        tic_catalog = pd.DataFrame()
+        for tic_id in tqdm(toi['TIC_ID'].unique()):
+            row_data = Catalogs.query_criteria(catalog="Tic", ID=tic_id)
+            row_data = row_data.to_pandas()
+            tic_catalog = tic_catalog.append(row_data)
+        tic_catalog = tic_catalog.reset_index(drop=True)
+        # Renaming ID column to make this consistent with Caltech TOI dataframe:
+        tic_catalog = tic_catalog.rename(columns={'ID': 'TIC_ID'})
+
+        # Getting all dataproducts for TESS Objects of Interest from STScI:
+        dataproducts = pd.DataFrame()
+        for tic_id in tqdm(toi['TIC_ID']):                
+            row_data = Observations.query_criteria(obs_collection="TESS",
+                                                target_name=tic_id)
+            row_data = row_data.to_pandas()
+            dataproducts = dataproducts.append(row_data)
+        dataproducts = dataproducts.reset_index(drop=True)
+        # Isolating TIC IDs (target_name) and dataURL values to get associated files:
+        dataproducts = dataproducts[['target_name', 'dataURL']]
+        # Renaming ID column to make this consistent with Caltech TOI dataframe:
+        dataproducts = dataproducts.rename(columns={'target_name': 'TIC_ID'})
+
+    except Exception as e:
+        print('Error importing data: ')
+        raise e
+    for index, row in dataproducts.iterrows():
+        # print(row[0], row[1])
+        new = Visual_Table(TIC_ID=row[0], dataURL=row[1])
+        DB.session.add(new)
+        DB.session.commit()
+    return
+
+
+def get_toi_data():
+    try: 
+        # Getting labelled TESS Objects of Interest dataframe from Caltech:
+        toi = pd.read_csv('https://exofop.ipac.caltech.edu/tess/' + 
+                    'download_toi.php?sort=toi&output=csv')
+    except Exception as e:
+        print('failed to import initial csv from caltech') 
+        raise e
+    try:
+        # Isolating columns we want:
+        toi = toi[['TIC ID',
+            'TOI',
+            'Epoch (BJD)',
+            'Period (days)',
+            'Duration (hours)',
+            'Depth (mmag)',
+            'Planet Radius (R_Earth)',
+            'Planet Insolation (Earth Flux)',
+            'Planet Equil Temp (K)',
+            'Planet SNR',
+            'Stellar Distance (pc)',
+            'Stellar log(g) (cm/s^2)',
+            'Stellar Radius (R_Sun)',
+            'TFOPWG Disposition',
+            ]]
+        toi.columns = toi.columns.str.replace(' ', '_')
+    except:
+        print('failed to filter df')
+    for index, row in toi.iterrows():
+        new = TOI_Table(TIC_ID=row[0], 
+                           TOI=row[1],
+                           Epoch=row[2],
+                           Period=row[3],
+                           Duration=row[4],
+                           Depth=row[5],
+                           Planet_Radius=row[6],
+                           Planet_Insolation=row[7],
+                           Planet_Equil_Temp=row[8],
+                           Planet_SNR=row[9],
+                           Stellar_Distance=row[10],
+                           Stellar_log_g=row[11],
+                           Stellar_Radius=row[12],
+                           TFOPWG_Disposition=row[13]
+                           )
+        DB.session.add(new)
+        DB.session.commit()
+    return
+
+
+def get_tic_catalog():
+    toi = pd.read_csv('https://exofop.ipac.caltech.edu/tess/' + 
+                    'download_toi.php?sort=toi&output=csv')
+    toi = toi[['TIC ID',
+            'TOI',
+            'Epoch (BJD)',
+            'Period (days)',
+            'Duration (hours)',
+            'Depth (mmag)',
+            'Planet Radius (R_Earth)',
+            'Planet Insolation (Earth Flux)',
+            'Planet Equil Temp (K)',
+            'Planet SNR',
+            'Stellar Distance (pc)',
+            'Stellar log(g) (cm/s^2)',
+            'Stellar Radius (R_Sun)',
+            'TFOPWG Disposition',
+            ]]
+    toi.columns = toi.columns.str.replace(' ', '_')
+    try:
+        tic_catalog = pd.DataFrame()
+        for tic_id in tqdm(toi['TIC_ID'].unique()):
+            row_data = Catalogs.query_criteria(catalog="Tic", ID=tic_id)
+            row_data = row_data.to_pandas()
+            tic_catalog = tic_catalog.append(row_data)
+        tic_catalog = tic_catalog.reset_index(drop=True)
+    except:
+        print('failed to import')
+    try:
+        # Renaming ID column to make this consistent with Caltech TOI dataframe:
+        tic_catalog = tic_catalog.rename(columns={'ID': 'TIC ID'})
+
+        # Isolating columns we want:
+        tic_catalog = tic_catalog[['TIC ID',
+                                'ra',
+                                'dec',
+                                'pmRA',
+                                'pmDEC',
+                                'plx',
+                                'gallong',
+                                'gallat',
+                                'eclong',
+                                'eclat',
+                                'Tmag',
+                                'Teff',
+                                'logg',
+                                'MH',
+                                'rad',
+                                'mass',
+                                'rho',
+                                'lum',
+                                'd',
+                                'ebv',
+                                'numcont',
+                                'contratio',
+                                'priority']]
+        tic_catalog.columns = tic_catalog.columns.str.replace(' ', '_')
+    except:
+            print('failed to filter columns')
+    for index, row in tic_catalog.iterrows():
+        new = TIC_Cat_Table(TIC_ID=row[0],
+                           ra=row[1],
+                           dec=row[2],
+                           pmRA=row[3],
+                           pmDEC=row[4],
+                           plx=row[5],
+                           gallong=row[6],
+                           gallat=row[7],
+                           eclong=row[8],
+                           eclat=row[9],
+                           Tmag=row[10],
+                           Teff=row[11],
+                           logg=row[12],
+                           MH=row[13],
+                           rad=row[14],
+                           mass=row[15],
+                           rho=row[16],
+                           lum=row[17],
+                           d=row[18],
+                           ebv=row[19],
+                           numcont=row[20],
+                           contratio=row[21],
+                           priority=row[22]
+                          )
+        DB.session.add(new)
+        DB.session.commit()
+    return
---
+++ App_TESS/light_curve.py
@@ -0,0 +1,148 @@
+import numpy as np
+import pandas as pd
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine
+from astropy.io import fits
+from .models import DB, Visual_Table
+import matplotlib.pyplot as plt
+from sqlalchemy import create_engine
+from astropy.io import fits
+
+# dataproducts = pd.read_csv('dataproducts_example.csv')
+# dataproducts = dataproducts.rename(columns={'TIC ID': 'TIC_ID'})
+
+# engine = create_engine('sqlite://', echo=False)
+
+# dataproducts.to_sql('dataproducts', con=engine)
+
+def get_urls(tic_id):
+    urls = Visual_Table.query.filter_by(TIC_ID=tic_id).all()
+    urls = [url[0] for url in urls]
+    return urls
+
+def get_lcs(urls):
+    return [url for url in urls if url.endswith('lc.fits')]
+
+def get_dvts(urls):
+    return [url for url in urls if url.endswith('dvt.fits')]
+
+def plot_lc(lc):
+    fits_file = ('https://mast.stsci.edu/api/v0.1/Download/file?uri=' + lc)
+            
+    with fits.open(fits_file, mode="readonly") as hdulist:
+        tess_bjds = hdulist[1].data['TIME']
+        sap_fluxes = hdulist[1].data['SAP_FLUX']
+        pdcsap_fluxes = hdulist[1].data['PDCSAP_FLUX']
+    
+    fig, ax = plt.subplots()
+
+    ax.plot(tess_bjds, pdcsap_fluxes, 'ko')
+
+    ax.set_ylabel("PDCSAP Flux (e-/s)")
+    ax.set_xlabel("Time (TBJD)")
+
+    return ax
+
+def plot_dvt(dvt):
+    fits_file = ('https://mast.stsci.edu/api/v0.1/Download/file?uri=' + dvt)
+    
+    with fits.open(fits_file, mode="readonly") as hdulist:
+
+        star_teff = hdulist[0].header['TEFF']
+        star_logg = hdulist[0].header['LOGG']
+        star_tmag = hdulist[0].header['TESSMAG']
+
+        period = hdulist[1].header['TPERIOD']
+        duration = hdulist[1].header['TDUR']
+        epoch = hdulist[1].header['TEPOCH']
+        depth = hdulist[1].header['TDEPTH']
+
+        times = hdulist[1].data['TIME']
+        phases = hdulist[1].data['PHASE']
+        fluxes_init = hdulist[1].data['LC_INIT']
+        model_fluxes_init = hdulist[1].data['MODEL_INIT']
+    
+    sort_indexes = np.argsort(phases)
+
+    fig, ax = plt.subplots(figsize=(12,4))
+
+    ax.plot(phases[sort_indexes], fluxes_init[sort_indexes], 'ko',
+           markersize=2)
+
+    ax.plot(phases[sort_indexes], model_fluxes_init[sort_indexes], '-r')
+
+    ax.set_ylabel("Flux (relative)")
+    ax.set_xlabel("Orbital Phase")
+
+    return ax
+
+# def save_all_lcs(tic_id):
+#     count = 0
+#     for lc in get_lcs(get_urls(tic_id)):
+#         plot_lc(lc)
+#         plt.savefig(fname=(images(str(tic_id) + '_lc_' + str(count))), format='png'))
+#         count += 1
+
+def save_all_dvts(tic_id):
+    count = 0
+    for dvt in get_dvts(get_urls(tic_id)):
+        plot_dvt(dvt)
+        plt.savefig(fname=(images/(str(tic_id) + '_dvt_' + str(count) + '.png')))
+        count += 1
+        
+# # This TIC has lcs:
+# save_all_lcs(149603524)
+
+# # This TIC does not:
+# save_all_lcs(149603523)
+
+# # This TIC has dvts:
+# save_all_dvts(149603524)
+
+# # This TIC does not:
+# save_all_dvts(149603523)
+
+
+
+# import numpy as np
+# import pandas as pd
+
+
+# # fetch Light Curve visual and basic data
+# def get_lightcurve(input_tic):
+#     # Getting urls for all dataproducts associated with TIC ID given by user
+#     try:
+
+#         # Next line need to become a DB query 
+#         # urls_for_input = dataproducts[dataproducts['TIC ID'] == input_tic][
+#         # 'dataURL'].tolist()
+#         try:
+#             urls_for_input = Visual_Table.query.filter(Visual_Table.TIC_ID == input_tic)
+#         except:
+#             print('failed to pull')
+        
+#         else:
+#             for url in urls_for_input:
+#                 count = 0 
+#                 fits_file = ('https://mast.stsci.edu/api/v0.1/Download/file?uri=' + url)
+                
+#                 # print(fits.info(fits_file), "\n")
+#                 # print(fits.getdata(fits_file, ext=1).columns)
+                
+#                 with fits.open(fits_file, mode="readonly") as hdulist:
+#                     tess_bjds = hdulist[1].data['TIME']
+#                     sap_fluxes = hdulist[1].data['SAP_FLUX']
+#                     pdcsap_fluxes = hdulist[1].data['PDCSAP_FLUX']
+                
+#                 fig, ax = plt.subplots()
+
+#                 ax.plot(tess_bjds, pdcsap_fluxes, 'ko')
+
+#                 ax.set_ylabel("PDCSAP Flux (e-/s)")
+#                 ax.set_xlabel("Time (TBJD)")
+
+#                 plt.savefig(fname=str(str(input_tic) + '_' + str(count) + '.png'))
+#     except:
+#         print('My Bad. Light Curve function is not working.')
+
+#     # return plt.show()
